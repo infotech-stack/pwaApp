1009f74ba0400469c8265b1b5d441e0ac041b436
ADMIN <ADMIN@DESKTOP-2PB0GNF>
2024-07-01 15:29:07 +0530

    Add project files.

---
+++ pwaApp/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace pwaApp.Services
+{
+    public interface IUserService
+    {
+        string Username { get; }
+        void SetUsername(string username);
+    }
+}
---
+++ pwaApp/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿namespace pwaApp.Services
+{
+    public class UserService : IUserService
+    {
+        private string _username;
+
+        public string Username => _username;
+
+        public void SetUsername(string username)
+        {
+            _username = username;
+        }
+    }
+}
